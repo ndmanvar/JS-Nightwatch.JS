6804a85085f9e2facb61d9a519e4acad2d5d289d
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2015-10-09 09:11:55 -0700

    Add test reporting log

--- custom_commands/customEnd.js
+++ custom_commands/customEnd.js
@@ -8,10 +8,13 @@ exports.command = function(result) {
     });
 
 	var sessionid = this.capabilities['webdriver.remote.sessionid'];
+  var jobName = this.options.desiredCapabilities.name;
 
   saucelabs.updateJob(sessionid, {
     passed: result === 0
   }, function(){});
+
+  console.log("SauceOnDemandSessionID=" + sessionid + " job-name=" + jobName);
 
   this.end();
 };
